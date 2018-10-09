a0d1a83c3ed55578bd672bf42b09e98ed674070d
Michael Beigelmacher <brooklynbagel@gmail.com>
2018-10-09 12:57:21 -0400

    Helper functions to format responses into a tabular format

--- R/survey_responses.R
+++ R/survey_responses.R
@@ -21,3 +21,88 @@ survey_responses <- function(id) {
 
   responses
 }
+
+single_choice <- function(x) {
+  responses <- x$responses %>%
+    tidyr::unnest()
+  if ("text" %in% colnames(responses)) {
+    responses <- responses %>%
+      dplyr::rename(text_other = .data$text)
+  } else {
+    responses <- responses %>%
+      tibble::add_column(text_other = NA_character_)
+  }
+  choices <- x$choices
+  dplyr::left_join(responses, choices, by = c("choice_id" = "id")) %>%
+    dplyr::mutate(text = dplyr::if_else(is.na(.data$text), .data$text_other, .data$text)) %>%
+    dplyr::select(.data$response_id, .data$text)
+}
+
+multiple_choice <- function(x) {
+  responses <- x$responses %>%
+    tidyr::unnest()
+  if ("text" %in% colnames(responses)) {
+    responses <- responses %>%
+      dplyr::rename(text_other = .data$text)
+  } else {
+    responses <- responses %>%
+      tibble::add_column(text_other = NA_character_)
+  }
+  choices <- x$choices
+  dplyr::left_join(responses, choices, by = c("choice_id" = "id")) %>%
+    dplyr::select(.data$response_id, .data$text, .data$text_other) %>%
+    tibble::add_column(selected = TRUE) %>%
+    tidyr::spread(.data$text, .data$selected)
+}
+
+open_ended_single <- function(x) {
+  x$responses %>%
+    tidyr::unnest()
+}
+
+open_ended_essay <- function(x) {
+  x$responses %>%
+    tidyr::unnest()
+}
+
+open_ended_multi <- function(x) {
+  responses <- x$responses %>%
+    tidyr::unnest()
+  rows <- x$rows
+  dplyr::left_join(responses, rows,
+    by = c("row_id" = "id"),
+    suffix = c("_response", "_row")
+  ) %>%
+    dplyr::select(.data$response_id, .data$text_row, .data$text_response) %>%
+    tidyr::spread(.data$text_row, .data$text_response)
+}
+
+matrix_rating <- function(x) {
+  responses <- x$responses %>%
+    tidyr::unnest()
+  choices <- x$choices
+  rows <- x$rows
+  if (all(choices$text == "") && all(rows$text == "")) {
+    responses %>%
+      dplyr::left_join(choices, by = c("choice_id" = "id")) %>%
+      dplyr::select(.data$response_id, .data$weight)
+  } else {
+    responses %>%
+      dplyr::left_join(choices, by = c("choice_id" = "id")) %>%
+      dplyr::left_join(rows, by = c("row_id" = "id"), suffix = c("_choice", "_row")) %>%
+      dplyr::select(.data$response_id, .data$text_choice, .data$text_row) %>%
+      tidyr::spread(.data$text_row, .data$text_choice)
+  }
+}
+
+matrix_ranking <- function(x) {
+  responses <- x$responses %>%
+    tidyr::unnest()
+  choices <- x$choices
+  rows <- x$rows
+  responses %>%
+    dplyr::left_join(choices, by = c("choice_id" = "id")) %>%
+    dplyr::left_join(rows, by = c("row_id" = "id"), suffix = c("_choice", "_row")) %>%
+    dplyr::select(.data$response_id, .data$text_row, .data$position_choice) %>%
+    tidyr::spread(.data$text_row, .data$position_choice)
+}
